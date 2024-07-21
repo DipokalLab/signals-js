232756de301bc657ba8aa46beafc927408f4e516
Dipokal HHJ <hhj@devent.kr>
2024-07-21 22:57:18 +0900

    feat: add impl of complex

--- lib/src/complex.rs
+++ lib/src/complex.rs
@@ -1,11 +1,11 @@
 
 pub mod complex {
     pub struct ComplexType {
-        pub real: u128,
-        pub imag: u128,
+        pub real: f64,
+        pub imag: f64,
     }
     
-    pub fn add(origin: ComplexType, target: ComplexType) -> ComplexType {
+    pub fn add(origin: &ComplexType, target: &ComplexType) -> ComplexType {
         let new_complex = ComplexType { 
             real: origin.real + target.real, 
             imag: origin.imag + target.imag 
@@ -14,10 +14,31 @@ pub mod complex {
         return new_complex;
     }
 
-    pub fn sub(origin: ComplexType, target: ComplexType) -> ComplexType {
+    pub fn sub(origin: &ComplexType, target: &ComplexType) -> ComplexType {
         let new_complex = ComplexType { 
             real: origin.real - target.real, 
             imag: origin.imag - target.imag 
+        };
+
+        return new_complex;
+    }
+
+    pub fn mul(origin: &ComplexType, target: &ComplexType) -> ComplexType {
+        let new_complex = ComplexType { 
+            real: origin.real * target.real - origin.imag * target.imag, 
+            imag: origin.real * target.imag + origin.imag * target.real
+        };
+
+        return new_complex;
+    }
+
+    pub fn cexp(origin: &ComplexType) -> ComplexType {
+        let x = 2.71828182845904523536028747135266250_f64;
+        let er = x.powf(origin.real);
+
+        let new_complex = ComplexType { 
+            real: er * f64::cos(origin.imag), 
+            imag: er * f64::sin(origin.imag)
         };
 
         return new_complex;
--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -23,20 +23,32 @@ pub fn test(name: &str) -> String {
     log(&format!("Hello, {}!", name));
     //alert(&format!("Hello, {}!", name));
 
-    let c1 = complex::complex::ComplexType {
-        imag: 1,
-        real: 3
+    let mut c1 = complex::complex::ComplexType {
+        imag: 1.0,
+        real: 3.0
     };
 
-    let c2 = complex::complex::ComplexType {
-        imag: 6,
-        real: 6
+    let mut c2 = complex::complex::ComplexType {
+        imag: 6.0,
+        real: 6.0
     };
 
-    let result = complex::complex::add(c1, c2);
+    let result = complex::complex::add(&c1, &c2);
+    let result1 = complex::complex::mul(&c1, &c2);
+    let result2 = complex::complex::sub(&c1, &c2);
+    let result3 = complex::complex::cexp(&c1);
 
     log(&result.imag.to_string());
     log(&result.real.to_string());
+
+    log(&result1.imag.to_string());
+    log(&result1.real.to_string());
+
+    log(&result2.imag.to_string());
+    log(&result2.real.to_string());
+
+    log(&result3.imag.to_string());
+    log(&result3.real.to_string());
 
     return format!("Hello, {}!", name);
 }
